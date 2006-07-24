d935bec967df96c789e70101a56e914ab632dbd1
obiwan314 <obiwan314@057a4675-e91d-c115-9ef9-cf11355ab8d8>
2006-07-24 18:02:49 +0000

    Added java.net headers to all of the framework package.

--- se/src/com/sun/ws/management/framework/transfer/TransferSupport.java
+++ se/src/com/sun/ws/management/framework/transfer/TransferSupport.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2005 Sun Microsystems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package com.sun.ws.management.framework.transfer;
 
 import java.util.List;
@@ -12,7 +28,6 @@ import org.dmtf.schemas.wbem.wsman._1.wsman.ObjectFactory;
 import org.dmtf.schemas.wbem.wsman._1.wsman.SelectorSetType;
 import org.dmtf.schemas.wbem.wsman._1.wsman.SelectorType;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xmlsoap.schemas.ws._2004._08.addressing.AttributedURI;
 import org.xmlsoap.schemas.ws._2004._08.addressing.EndpointReferenceType;
 import org.xmlsoap.schemas.ws._2004._08.addressing.ReferenceParametersType;
