4d2a8e2b56c2cc1462fac8143014ccc5aa0a5bbd
obiwan314 <obiwan314@057a4675-e91d-c115-9ef9-cf11355ab8d8>
2006-09-07 15:00:46 +0000

    Shell script was using $ instead of %%. Fixed.

--- tools/samples/ant_install.bat
+++ tools/samples/ant_install.bat
@@ -1,8 +1,8 @@
-copy ..\lib\activation.jar $ANT_HOME\lib
-copy ..\lib\jaxb-api.jar $ANT_HOME\lib
-copy ..\lib\jaxb-impl.jar $ANT_HOME\lib
-copy ..\lib\jaxb-xjc.jar $ANT_HOME\lib
-copy ..\lib\jsr173_1.0_api.jar $ANT_HOME\lib
-copy ..\lib\velocity-*.jar $ANT_HOME\lib
-copy ..\lib\wiseman-tools.jar $ANT_HOME\lib
-copy ..\lib\wsdl4j.jar $ANT_HOME\lib
+copy ..\lib\activation.jar %ANT_HOME%\lib
+copy ..\lib\jaxb-api.jar %ANT_HOME%\lib
+copy ..\lib\jaxb-impl.jar %ANT_HOME%\lib
+copy ..\lib\jaxb-xjc.jar %ANT_HOME%\lib
+copy ..\lib\jsr173_1.0_api.jar %ANT_HOME%\lib
+copy ..\lib\velocity-*.jar %ANT_HOME%\lib
+copy ..\lib\wiseman-tools.jar %ANT_HOME%\lib
+copy ..\lib\wsdl4j.jar %ANT_HOME%\lib
