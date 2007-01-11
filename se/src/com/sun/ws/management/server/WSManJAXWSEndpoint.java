3c4f6ad7d970992f4167d7ba7df8db1b0b92d64c
jfdenise <jfdenise@057a4675-e91d-c115-9ef9-cf11355ab8d8>
2007-01-11 13:20:46 +0000

    Wiseman on top of JAX-WS 2.0. Merge with jaxws_branch Main changes: - Removal of JAXB binaries from CVS.   You will have to copy jaxws jars (you can download them from https://jax-ws.dev.java.net/jax-ws-20-fcs/) in lib/jaxws directory.  (Planing to see how we can integrate the Maven java.net repository to express dependency we have on JAX-WS project.) - Source code impact. Quite a lot but mainly rework of WsManServlet, introduced new WSManAgent and WSManJAXWSEndpoint.   Rework of initialization, removal of various initialization() methods.   New Message construction based on a SOAPMessage. - Schemas location.They are now located in com.sun.ws.management.resources.schemas.   They can be found as Resource from current ClassLoader. It is no more linked to Servlet.   Custom schemas can still be provided thanks to ServletContext (as before)   The schemas have been renamed, the ordering based on name has been replaced by a list in wiseman.properties file. - JAXB Generation. Removal of the files from the repository.   Generated file should be generated with the release of JAX-WS you selected.   If we let the generated code in the CVS repository, we could ran into future compatibility problems.   You have to call "generate-jaxb" manually. - Build impact. New targets to build and deploy jax-ws compliant war (named wisman-jaxws.war) - Tests. Removal of the automatic copy of JAXB/SAAJ jar in the tomcat /shared/lib.   This is something that is nice but can easily break existing configuration.   All existing tests and interop scenarii can be run with a Servlet based agent or with a JAX-WS one.   Choose your target : "test" or "test-jaxws". The single difference between these 2 targets is deployment of the right war.   J2SE deployment. WiseMan JAX-WS Endpoint can be deployed in JAX-WS http server (no more need of Servlet container),   the build targets are currently commented due to Servlet dependency in Tests (If someone wants to rework a little bit handlers,   we could have all tests passing indifferently on top of J2SE or J2EE!!!!). To play with such deployment, uncomment following   ant build targets "deploy-j2se-jaxws" to start the agent side and "test-j2se-jaxws" to start the client side). - misc: Commented the test.foo package from binding.properties. - Trace. Removal of logging configuration file loading. Addition of a logging.properties file that is   packaged in WEB-INF/classes in order to generate in <catalina-home>/logs a file that contains all wiseman traces.   This is very useful when you are debugging tests. The generated file name is wiseman-<date of the day>.out

---
+++ se/src/com/sun/ws/management/server/WSManJAXWSEndpoint.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2005 Sun Microsystems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id: WSManJAXWSEndpoint.java,v 1.2 2007-01-11 13:19:06 jfdenise Exp $
+ */
+
+package com.sun.ws.management.server;
+
+import com.sun.ws.management.InternalErrorFault;
+import com.sun.ws.management.Management;
+import com.sun.ws.management.Message;
+import com.sun.ws.management.transport.ContentType;
+import com.sun.ws.management.xml.XmlBinding;
+import java.net.URL;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.security.auth.Subject;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.ws.WebServiceContext;
+import java.lang.management.ManagementFactory;
+import javax.annotation.Resource;
+import javax.management.MBeanServer;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.Provider;
+import javax.xml.ws.Service;
+import javax.xml.ws.ServiceMode;
+import javax.xml.ws.WebServiceProvider;
+import javax.xml.ws.handler.MessageContext;
+import org.xml.sax.SAXException;
+
+/**
+ * JAX-WS Compliant endpoint.
+ */
+@WebServiceProvider()
+@ServiceMode(value=Service.Mode.MESSAGE)
+public class WSManJAXWSEndpoint implements Provider<SOAPMessage> {
+    
+    private static final Logger LOG = Logger.getLogger(WSManJAXWSEndpoint.class.getName());
+    private WSManAgent agent;
+    // JAXWS Context injection
+    @Resource
+    private WebServiceContext context;
+    
+    /**
+     * JAX-WS Endpoint constructor
+     */
+    public WSManJAXWSEndpoint() {
+    }
+    
+    public SOAPMessage invoke(SOAPMessage message) {
+        Management request = null;
+        HandlerContext ctx = null;
+        
+        try {
+            request = new Management(message);
+            
+            Principal principal = getWebServiceContext().getUserPrincipal();
+            String contentType = ContentType.DEFAULT_CONTENT_TYPE.getMimeType();
+            String encoding = request.getContentType() == null ? null : request.getContentType().getEncoding();
+            String url = request.getTo();
+            Object servletContext = getWebServiceContext().getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+            WebServiceContext webctx = getWebServiceContext();
+            Map<String, Object> props = new HashMap<String, Object>(1);
+            props.put(HandlerContext.SERVLET_CONTEXT, servletContext);
+            props.put(HandlerContext.JAX_WS_CONTEXT, webctx);
+            
+            
+             if (LOG.isLoggable(Level.FINE))
+                LOG.fine("Context properties : " + " contentType " + contentType + 
+                        ", encoding " + encoding + ", url " + url + ", servletContext"
+                        + servletContext + ", webctx " + 
+                        webctx);
+
+            ctx = new HandlerContextImpl(principal, contentType, encoding, url, props,
+                    getAgent().getProperties());
+            Message reply = getAgent().handleRequest(request, ctx);
+            return reply.getMessage();
+        }catch(Exception ex) {
+            try {
+                Management response = new Management();
+                response.setFault(new InternalErrorFault(ex.getMessage()));
+                return response.getMessage();
+            }catch(Exception ex2) {
+                // We can't handle the internal error.
+                throw new RuntimeException(ex2.getMessage());
+            }
+        }
+    }
+    
+    private synchronized WSManAgent getAgent() throws SAXException {
+        if(agent == null)
+            agent = createWSManAgent();
+        return agent;
+    }
+    
+    /*
+     * In case this class is extended, Annotations @WebServiceProvider()
+     * @ServiceMode(value=Service.Mode.MESSAGE) @Resource must be set on the extended class
+     * or JAX-WS will not recognize the endpoint as being a valid JAX-WS endpoint.
+     * This method is used to retrieve the injected resource of the extended class.
+     */
+    protected WebServiceContext getWebServiceContext() {
+        return context;
+    }
+    
+    protected WSManAgent createWSManAgent() throws SAXException {
+        // It is an extension of WSManAgent to handle Reflective Dispatcher
+        return new WSManReflectiveAgent();
+    }
+}
---
+++ se/src/com/sun/ws/management/server/WSManReflectiveAgent.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2005 Sun Microsystems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id: WSManReflectiveAgent.java,v 1.2 2007-01-11 13:19:06 jfdenise Exp $
+ */
+
+package com.sun.ws.management.server;
+
+import com.sun.ws.management.Management;
+import java.io.IOException;
+import javax.xml.bind.JAXBException;
+import javax.xml.soap.SOAPException;
+import javax.xml.transform.Source;
+import org.xml.sax.SAXException;
+
+/**
+ * Agent that delegates to a Reflective Request Dispatcher
+ */
+public class WSManReflectiveAgent extends WSManAgent {
+    public WSManReflectiveAgent() throws SAXException {
+        this(null);
+    }
+    
+    public WSManReflectiveAgent(Source[] schemas, String... customPackages) throws SAXException {
+        super(schemas, customPackages);
+    }
+     
+    protected RequestDispatcher createDispatcher(final Management request,
+            final HandlerContext context) throws SOAPException, JAXBException, IOException {
+        return new ReflectiveRequestDispatcher(request, context);
+    }
+}
+
