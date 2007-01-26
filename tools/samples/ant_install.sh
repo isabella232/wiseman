908e176d4a5b1aa57657cbd294a67cd82572ef1f
denis_rachal <denis_rachal@057a4675-e91d-c115-9ef9-cf11355ab8d8>
2007-01-26 16:42:32 +0000

    Issue number:  61 Obtained from: Submitted by:  denis_rachal Reviewed by:
    Remove scripts that are no longer needed.

--- tools/samples/ant_install.sh
+++
@@ -1,8 +0,0 @@
-cp ../lib/activation.jar $ANT_HOME/lib
-cp ../lib/jaxb-api.jar $ANT_HOME/lib
-cp ../lib/jaxb-impl.jar $ANT_HOME/lib
-cp ../lib/jaxb-xjc.jar $ANT_HOME/lib
-cp ../lib/jsr173_1.0_api.jar $ANT_HOME/lib
-cp ../lib/velocity-*.jar $ANT_HOME/lib
-cp ../lib/wiseman-tools.jar $ANT_HOME/lib
-cp ../lib/wsdl4j.jar $ANT_HOME/lib
