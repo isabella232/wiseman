dcd872e4ca0f4199245e5bbab1f43d4febcc651a
nbeers <nbeers@057a4675-e91d-c115-9ef9-cf11355ab8d8>
2007-05-24 18:39:57 +0000

    Fix classpath based on new install directory location

--- tools/wise.bat
+++ tools/wise.bat
@@ -1,5 +1,5 @@
 
 rem set CLASSPATH
-set CP=wiseman-core.jar;wiseman-tools.jar;jaxws\saaj-api.jar;jaxws\jaxb-api.jar;jaxws\saaj-impl.jar;jaxws\jaxb-impl.jar;jaxws\jsr173_api.jar
+set CP=..\lib\wiseman-core.jar;..\lib\wiseman-tools.jar;..\lib\jaxws\saaj-api.jar;..\lib\jaxws\jaxb-api.jar;..\lib\jaxws\saaj-impl.jar;..\lib\jaxws\jaxb-impl.jar;..\lib\jaxws\jsr173_api.jar
 
 java -cp %CP% com.sun.ws.management.tools.WisemanCmdLine %*
