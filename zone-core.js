a65b36003c743fc2ca8f71f3f81cbb030672f32d
johntollefsrud <johntollefsrud@057a4675-e91d-c115-9ef9-cf11355ab8d8>
2008-02-06 04:23:54 +0000

    Left side project nav for Wiseman

---
+++ zone-core.js
@@ -0,0 +1,38 @@
+var zones = [
+    {
+        title: "Metro",
+        href:  "https://metro.dev.java.net/",
+    },
+    {
+        title: "JAX-WS",
+        href:  "https://jax-ws.dev.java.net/"
+    },
+    {
+        title: "WSIT",
+        href:  "https://wsit.dev.java.net/"
+    },
+    {
+        title: "JAXB",
+        href:  "https://jaxb.dev.java.net/"
+    },
+    {
+        title: "JAX-WS Commons",
+        href:  "https://jax-ws-commons.dev.java.net/"
+    },
+    {
+        title: "Libraries",
+        href:  "https://metro.dev.java.net/libraries/"
+    },
+    {
+        title: "Policy",
+        href:  "https://policy.dev.java.net/"
+    },
+    {
+        title: "Test Harness",
+        href:  "https://ws-test-harness.dev.java.net/"
+    },
+    {
+        title: "Wiseman",
+        href:  "https://wiseman.dev.java.net/"
+    }
+];
