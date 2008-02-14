6eea15ef9b92a9363c3f574b2504389e2c371772
denis_rachal <denis_rachal@057a4675-e91d-c115-9ef9-cf11355ab8d8>
2008-02-14 08:25:03 +0000

    Enhancement to trace the incoming request at the server.

--- se/src/com/sun/ws/management/server/WSManAgent.java
+++ se/src/com/sun/ws/management/server/WSManAgent.java
@@ -19,6 +19,18 @@
  ** Nancy Beers (nancy.beers@hp.com), William Reichardt
  **
  **$Log: not supported by cvs2svn $
+ **Revision 1.22  2007/11/30 14:32:38  denis_rachal
+ **Issue number:  140
+ **Obtained from:
+ **Submitted by:  jfdenise
+ **Reviewed by:
+ **
+ **WSManAgentSupport and WSEnumerationSupport changed to coordinate their separate threads when handling wsman:OperationTimeout and wsen:MaxTime timeouts. If a timeout now occurs during an enumeration operation the WSEnumerationSupport is notified by the WSManAgentSupport thread. WSEnumerationSupport saves any items collected from the EnumerationIterator in the context so they may be fetched by the client on the next pull. Items are no longer lost on timeouts.
+ **
+ **Tests were added to correctly test this functionality and older tests were updated to properly test timeout functionality.
+ **
+ **Additionally some tests were updated to make better use of the XmlBinding object and improve performance on testing.
+ **
  **Revision 1.21  2007/11/09 12:33:34  denis_rachal
  **Performance enhancements that better reuse the XmlBinding.
  **
@@ -39,7 +51,7 @@
  **Add HP copyright header
  **
  **
- * $Id: WSManAgent.java,v 1.22 2007-11-30 14:32:38 denis_rachal Exp $
+ * $Id: WSManAgent.java,v 1.22.8.1 2008-02-14 08:25:03 denis_rachal Exp $
  */
 
 package com.sun.ws.management.server;
@@ -133,6 +145,7 @@ public abstract class WSManAgent extends WSManAgentSupport {
      */
     public Message handleRequest(final Management request, final HandlerContext context) {
         try {
+        	logMessage(LOG, request);
             SAAJMessage req = new SAAJMessage(request);
             SAAJMessage resp = new SAAJMessage(new Management());
             
